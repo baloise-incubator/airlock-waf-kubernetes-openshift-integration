e2c1e4029111a1e8f1d110f59130701a55754c59
Markus Tiede <markus.tiede@baloise.ch>
2020-09-24 18:07:36 +0200

    Add test to reproduce issue

--- event-listener/src/test/java/com/airlock/waf/client/DebugTests.java
+++ event-listener/src/test/java/com/airlock/waf/client/DebugTests.java
@@ -1,14 +1,30 @@
 package com.airlock.waf.client;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.airlock.waf.client.config.rs.client.AirlockWAFClient;
 
 @SpringBootTest(classes = Application.class)
 class DebugTests {
 
+  private AirlockWAFClient client;
+  private Context context;
+
+  @Autowired
+  public DebugTests(AirlockWAFClient client, Context context) {
+    this.client = client;
+    this.context = context;
+  }
+
   @Test
-  void contextLoads() {
-    System.out.println();
+  @Disabled
+  void debug() {
+    String cookie = client.session().create();
+    client.configuration().loadBaseConfig(cookie);
+    String backendGroupId = client.backendgroup().getKubernetesBackendGroupId(cookie);
   }
 
 }
