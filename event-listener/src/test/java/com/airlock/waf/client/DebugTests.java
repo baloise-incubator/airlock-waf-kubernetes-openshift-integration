6b9154bf08fdee7a0186c87bfa0760908d2baa05
Markus Tiede <markus.tiede@baloise.ch>
2020-09-24 15:39:39 +0200

    Add (very) basic test infra

---
+++ event-listener/src/test/java/com/airlock/waf/client/DebugTests.java
@@ -0,0 +1,14 @@
+package com.airlock.waf.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = Application.class)
+class DebugTests {
+
+  @Test
+  void contextLoads() {
+    System.out.println();
+  }
+
+}
